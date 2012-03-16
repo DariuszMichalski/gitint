66ce5f71521895304f9baad5ea8253db0da537dc
Dariusz Michalski <dariusz.michalski@useo.pl>
2012-03-16 13:19:23 +0100

    Change test payload to real payload from github-services. Modify post action which handles requests from github-services (also it's name is changed and now handles project_id parameter)

--- api.rb
+++ api.rb
@@ -11,9 +11,8 @@ class API < Sinatra::Base
   # -------------------------------------------- #
 
   helpers do
-    def payload_correct?(payload)
-      payload["token"] && payload["project_id"] && payload["commit"]["message"] &&
-        payload["github_user"]["username"] && payload["github_user"]["email"]
+    def data_correct?(data)
+      data["token"] && data["payload"] && data["payload"]["commits"]
     rescue
       false
     end
@@ -25,15 +24,13 @@ class API < Sinatra::Base
     cp = CommitParser.new("[ In Progress  #321] my commit message")  #(last_commit_message)
   end
 
-  post '/' do # github-services
-    puts "\n* Got POST message from github-services *"
-    puts "JSON: " + params[:data]
-    puts "Parsed: " + JSON.parse(params[:data]).inspect
-  end
+  post '/project/:project_id' do # github-services
+    # puts "\n* Got POST message from github-services * project_id: #{params[:project_id]}"
+    # puts "\nJSON:\n" + params[:data]
+    # puts "\nParsed:\n" + JSON.parse(params[:data]).inspect
 
-  post '/push' do
-    if push = JSON.parse(params[:payload]) and payload_correct?(push)
-      last_commit_message = push["commit"]["message"] # get last commit message
+    if params[:project_id] and data = JSON.parse(params[:data]) and data_correct?(data)
+      last_commit_message = data["payload"]["commits"].last["message"] # get last commit message
       # parse commit message
       cp = CommitParser.new(last_commit_message)
       # this is just for testing
@@ -44,6 +41,12 @@ class API < Sinatra::Base
       status 400 # incorrect payload
     end
 
+  end
+
+  post '/push' do
+    # payload = JSON.parse(params[:payload])
+    # puts "payload: #{payload}"
+    status 200
   end
 
 end
--- spec/snd_commit_parser_spec.rb
+++ spec/snd_commit_parser_spec.rb
@@ -1,23 +1,157 @@
 require 'commit_parser'
 
 describe "Handle POST request with given correct payload" do
+  
+  let(:project_id) { "123" } 
+  
+  let(:data) do # this is the payload sent via github-services
+    '{
+      "payload":{
+        "pusher":{
+          "name":"none"
+        },
+        "repository":{
+          "name":"gitint",
+          "size":160,
+          "has_wiki":true,
+          "created_at":"2012/03/14 02:46:50 -0700",
+          "private":false,
+          "watchers":1,
+          "fork":false,
+          "url":"https://github.com/DariuszMichalski/gitint",
+          "language":"Ruby",
+          "pushed_at":"2012/03/15 15:59:48 -0700",
+          "has_downloads":true,
+          "open_issues":0,
+          "homepage":"",
+          "has_issues":true,
+          "forks":1,
+          "description":"Github Integration",
+          "owner":{
+            "name":"DariuszMichalski",
+            "email":"dariusz.michalski@useo.pl"
+          }
+        },
+        "forced":false,
+        "head_commit":{
+          "modified":[
 
-  let(:payload) do # this is the payload sent via github-services
-    {
-      :token => "ZXCSAJ12DD1212ACCEFBT",
-      :project_id => 1234,
-      :commit => {
-        :message => "[#1234] this comment will appear in Agile Bench Story 1234"
+          ],
+          "added":[
+            "services/agile_bench.rb"
+          ],
+          "author":{
+            "name":"Dariusz Michalski",
+            "username":"DariuszMichalski",
+            "email":"dariusz.michalski@useo.pl"
+          },
+          "timestamp":"2012-03-15T15:59:38-07:00",
+          "removed":[
+
+          ],
+          "url":"https://github.com/DariuszMichalski/gitint/commit/d477937fb1fef438b2298725423c1bd117010885",
+          "id":"d477937fb1fef438b2298725423c1bd117010885",
+          "distinct":true,
+          "message":"AgileBench - github-services file",
+          "committer":{
+            "name":"Dariusz Michalski",
+            "username":"DariuszMichalski",
+            "email":"dariusz.michalski@useo.pl"
+          }
+        },
+        "after":"d477937fb1fef438b2298725423c1bd117010885",
+        "deleted":false,
+        "ref":"refs/heads/master",
+        "commits":[
+          {
+            "modified":[
+              "spec/snd_commit_parser_spec.rb"
+            ],
+            "added":[
+
+            ],
+            "author":{
+              "name":"Dariusz Michalski",
+              "username":"DariuszMichalski",
+              "email":"dariusz.michalski@useo.pl"
+            },
+            "timestamp":"2012-03-15T10:46:15-07:00",
+            "removed":[
+
+            ],
+            "url":"https://github.com/DariuszMichalski/gitint/commit/7f09f6eef2de765459be546c612ece131df2f075",
+            "id":"7f09f6eef2de765459be546c612ece131df2f075",
+            "distinct":true,
+            "message":"New spec file",
+            "committer":{
+              "name":"Dariusz Michalski",
+              "username":"DariuszMichalski",
+              "email":"dariusz.michalski@useo.pl"
+            }
+          },
+          {
+            "modified":[
+              "api.rb"
+            ],
+            "added":[
+
+            ],
+            "author":{
+              "name":"Dariusz Michalski",
+              "username":"DariuszMichalski",
+              "email":"dariusz.michalski@useo.pl"
+            },
+            "timestamp":"2012-03-15T15:02:43-07:00",
+            "removed":[
+
+            ],
+            "url":"https://github.com/DariuszMichalski/gitint/commit/e7eeb7f6856548d918202b5a657be0cb54611678",
+            "id":"e7eeb7f6856548d918202b5a657be0cb54611678",
+            "distinct":true,
+            "message":"Post route for testing github-services",
+            "committer":{
+              "name":"Dariusz Michalski",
+              "username":"DariuszMichalski",
+              "email":"dariusz.michalski@useo.pl"
+            }
+          },
+          {
+            "modified":[
+
+            ],
+            "added":[
+              "services/agile_bench.rb"
+            ],
+            "author":{
+              "name":"Dariusz Michalski",
+              "username":"DariuszMichalski",
+              "email":"dariusz.michalski@useo.pl"
+            },
+            "timestamp":"2012-03-15T15:59:38-07:00",
+            "removed":[
+
+            ],
+            "url":"https://github.com/DariuszMichalski/gitint/commit/d477937fb1fef438b2298725423c1bd117010885",
+            "id":"d477937fb1fef438b2298725423c1bd117010885",
+            "distinct":true,
+            "message":"[#1234] this comment will appear in Agile Bench Story 1234",
+            "committer":{
+              "name":"Dariusz Michalski",
+              "username":"DariuszMichalski",
+              "email":"dariusz.michalski@useo.pl"
+            }
+          }
+        ],
+        "before":"25e6cc398ff2c26af06444e826887ad05c37b29a",
+        "compare":"https://github.com/DariuszMichalski/gitint/compare/25e6cc3...d477937",
+        "created":false
       },
-      :github_user => {
-          :username => "SomeUserName",
-          :email => "email@example.com"
-      }
-    }
+      "token":"test_token"
+    }'
   end
 
-  let(:wrong_payload) do # this is a bad payload
-    { }
+  let(:wrong_data) do # this is a bad payload
+    { }.to_json
   end
 
   context "Commit Parser" do
@@ -26,30 +160,32 @@ describe "Handle POST request with given correct payload" do
     #end
 
     it "should be initialized correctly after receiving correct payload" do
-      post "/push", params = {:payload => payload.to_json } do
-        #@commit_parser.should_receive(:new).with(payload[:commit][:message]).and_return(true) # SOMETHING WRONG WHERE
+      post "/push", params = { :payload => data } do
+        last_response.status.should == 200
       end
     end
 
     it "should get POST payload from gihub and return parsed data" do
-      post "/push", params = { :payload => payload.to_json }
-      parsed_data = {:complete_message => "[#1234] this comment will appear in Agile Bench Story 1234",
-                     :commit_message   => "this comment will appear in Agile Bench Story 1234",
-                     :stories          => ["1234"],
-                     :workflow_change  => "" }
-      last_response.status.should == 200
-      last_response.body.should == parsed_data.to_json
+      post "/project/#{project_id}", params = { :data => data } do
+        parsed_data = {:complete_message => "[#1234] this comment will appear in Agile Bench Story 1234",
+                       :commit_message   => "this comment will appear in Agile Bench Story 1234",
+                       :stories          => ["1234"],
+                       :workflow_change  => "" }
+        last_response.body.should == parsed_data.to_json
+        last_response.status.should == 200
+      end
     end
+    
   end
 
   context "Response statuses" do
     it "should work fine with correct payload" do
-      post "/push", params = {:payload => payload.to_json } do
+      post "/project/#{project_id}", params = { :data => data } do
         last_response.status.should == 200
       end
     end
     it "should reject incorrect payload" do
-      post "/push", params = {:payload => wrong_payload.to_json } do
+      post "/project/#{project_id}", params = { :data => wrong_data } do
         last_response.status.should == 400
       end
     end
